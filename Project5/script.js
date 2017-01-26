4090bf3be51fc5390497c9d50a5560dcae4e96eb
been2001 <been2001@yahoo.com>
2017-01-25 19:04:48 -0600

    project5

---
+++ Project5/script.js
@@ -0,0 +1,12 @@
+var movie = {
+    name: "Avengers",
+    runtimeInMins: 143,
+    releaseYear: 2012,
+    director: "Abdifatah"
+};
+
+function correctedObject(arg) {
+	console.log(arg);
+}
+
+correctedObject(movie);
